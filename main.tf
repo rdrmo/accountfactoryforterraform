8f811d834b29c495e643600aaa962c7bec19d0f4
rdrmo <150148129+rdrmo@users.noreply.github.com>
2023-11-07 11:40:17 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "aft" {
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  ct_management_account_id    = var.ct_management_account_id
+  log_archive_account_id      = var.log_archive_account_id
+  audit_account_id            = var.audit_account_id
+  aft_management_account_id   = var.aft_management_account_id
+  ct_home_region              = var.ct_home_region
+  tf_backend_secondary_region = var.tf_backend_secondary_region
+
+  vcs_provider                                  = "github"
+  terraform_distribution                        = "tfc"
+  terraform_org_name                            = "amazon_rdrmo"
+  terraform_api_endpoint                        = "https://app.terraform.io/api/v2/"
+  terraform_token                               = var.terraformtoken
+  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
+  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
+  global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
+  account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+}

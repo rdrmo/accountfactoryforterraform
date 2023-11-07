06d05031dfa38d6f11597661682bc457a405a953
rdrmo <150148129+rdrmo@users.noreply.github.com>
2023-11-07 12:07:24 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,14 @@
 provider "tfc" {
   #hostname = var.hostname # Optional, defaults to Terraform Cloud `app.terraform.io`
-  token    = var.token
+  token    = var.terraformtoken
   version  = "~> 0.49.2"
 }
 
 # Create an organization
 #resource "tfe_organization" "org" {
   # ...
+}
+
+provider "aws" {
+  region = var.aws_region
 }

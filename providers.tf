9bb95ba21b550f73ba564ceb9836151eb683d5bc
rdrmo <150148129+rdrmo@users.noreply.github.com>
2023-11-07 12:16:39 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,8 +1,18 @@
-provider "tfc" {
-  #hostname = var.hostname # Optional, defaults to Terraform Cloud `app.terraform.io`
-  token    = var.terraformtoken
-  version  = "~> 0.49.2"
+terraform {
+  cloud {
+    organization = "Amazon_rdrmo"
+
+    workspaces {
+      name = "Example"
+    }
+  }
 }
+
+#provider "tfc" {
+#  #hostname = var.hostname # Optional, defaults to Terraform Cloud `app.terraform.io`
+#  token    = var.terraformtoken
+#  version  = "~> 0.49.2"
+#}
 
 terraform {
   required_providers {

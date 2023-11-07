3f03a5ac8d2b9b1e34c4ae65a462727e56e7e943
rdrmo <150148129+rdrmo@users.noreply.github.com>
2023-11-07 13:32:59 -0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,29 @@
-terraform {
-  required_version = ">=0.15.0"
+#terraform {
+#  required_version = ">=0.15.0"
 
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = ">= 4.4"
+#    }
+#  }
+#}
+
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.4"
+      version = "~> 3.28.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
     }
   }
+
+  required_version = ">= 0.15.0"
 }

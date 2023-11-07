691fe19eec79b1de65992360c7ed6b4dd255b31e
rdrmo <150148129+rdrmo@users.noreply.github.com>
2023-11-07 11:28:47 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "ct_management_account_id" {
+  type = string
+  default = "291045144839"
+}
+
+variable "log_archive_account_id" {
+  type = string
+  default = "716856377128"
+}
+
+variable "audit_account_id" {
+  type = string
+  default = "354111917189"
+}
+
+variable "aft_management_account_id" {
+  type = string
+  default = "422822914274"
+}
+
+variable "github_username" {
+  type = string
+  default = "caprica64"
+}
+
+variable "ct_home_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "tf_backend_secondary_region" {
+  type = string
+  default = "us-east-2"
+}
+
+variable "terraformtoken" {
+  type = string
+}
